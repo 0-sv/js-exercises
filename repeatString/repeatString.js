2d13bdb307d37a4429b47164d6e9675a2e9a7ddf
Sebastiaan Verbeek <contact@svatwork.com>
2020-03-25 18:51:59 +0700

    Head recursion looks elegant

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
-}
+const repeatString = function(str, x) { 
+    if (x > 1) return str + repeatString(str, x - 1);
+    else if (x === 1) return str;
+    else if (x === 0) return '';
+    else if (x < 0) return 'ERROR';
+} 
 
 module.exports = repeatString
