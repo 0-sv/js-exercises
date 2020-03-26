2ec844f9b9ec03b5702200d1c7b5050bc9daea7b
Sebastiaan Verbeek <contact@svatwork.com>
2020-03-26 13:06:38 +0700

    Fibo in bash

--- fibonacci/fibonacci.sh
+++ fibonacci/fibonacci.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 function fibo() {
-    return $1
+    if [ $1 -eq 0 ]
+    then
+        echo 0
+    elif [ $1 -eq 1 ]
+    then
+        echo 1
+    elif [ $1 -gt 1 ]
+    then
+        lhs=$(fibo $(( $1 - 1)))
+        rhs=$(fibo $(( $1 - 2)))
+        echo $(( $lhs + $rhs ))
+    fi
 }
 
-fibo 1
+fibo 8
 echo $?
