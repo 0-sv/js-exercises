31e529cac129b246dbad276460ba6d4ec5b01d6c
Sebastiaan Verbeek <contact@svatwork.com>
2020-03-26 12:50:57 +0700

    Base fibo function without type checking

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS";
+    else if (x === 0) return 0;
+    else if (x === 1) return 1;
+    else return fibonacci(x - 1) + fibonacci(x - 2);
 }
 
 module.exports = fibonacci
+
+// x = 6:
+// lhs = fibonacci(5), rhs = fibonacci(4)
+// lhs = fibonacci(4) + fibonacci(3), rhs = fibonacci(3) + fibonacci(2)
+// lhs = fibonacci(3) + fibonacci(2) + fibonacci(1) + fibonacci(0), rhs = fibonacci(2) + fibonacci(1) + fibonacci(1) + fibonacci(0)
+// ...
+// return 5 + 3
