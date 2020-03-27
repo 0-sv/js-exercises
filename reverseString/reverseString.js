992a28a56d518a9768dbead39727527d061d126f
Sebastiaan Verbeek <contact@svatwork.com>
2020-03-27 18:42:41 +0700

    Recursive reversestring

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str.length === 2) return str.charAt(1) + str.charAt(0);
+    else if (str.length === 3) return str.charAt(2) + str.charAt(1) + str.charAt(0);
+    else return reverseString(str.slice(str.length/2, str.length)) + 
+        reverseString(str.slice(0, str.length/2));
 }
 
 module.exports = reverseString
