ffd20f3c98f292b859dfc4a7702f973fe44befa6
Sebastiaan Verbeek <contact@svatwork.com>
2020-03-28 18:09:41 +0700

    First test passes

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rest) {
+    if (arr.length === 1) return (arr[0] === rest[0] ? [] : arr);
+    else return removeFromArray(arr.slice(0, arr.length/2), rest[0])
+        .concat(removeFromArray(arr.slice(arr.length/2, arr.length), rest[0]));
 }
 
+console.log(removeFromArray([1, 2, 3, 4], 3))
 module.exports = removeFromArray
