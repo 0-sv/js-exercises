958f1194c46d2e8f78309c776d2730e098143b3b
Sebastiaan Verbeek <contact@svatwork.com>
2020-03-28 21:01:03 +0700

    SumAll written iterative

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(m, n) {
+    if (m < 0 || n < 0) return "ERROR";
+    if (typeof(m) !== typeof(0) || typeof(n) !== typeof(0)) return "ERROR";
+    
+    result = 0;
+    for (let i = Math.min(n, m); i <= Math.max(m, n); ++i) result += i;
+    return result; 
 }
 
 module.exports = sumAll
